a06f6e3e5fd9385affac7af4448ec9efff1e3d48
Luciano Fronza <luciano.fronza@gmail.com>
2025-08-22 14:08:36 -0300

    fix: resolve ESLint configuration issues in frontend
    - Added eslint-plugin-react for React support
    - Fixed TypeScript ESLint plugin configuration
    - Renamed Comment interface to PrComment to avoid global conflict
    - Added proper React environment and parser options
    - Resolves 'React is not defined' and TypeScript rule errors

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -1,20 +1,40 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { 
+    browser: true, 
+    es2020: true,
+    node: true,
+    es6: true,
+  },
   extends: [
     'eslint:recommended',
+    'plugin:react/recommended',
     'plugin:react-hooks/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ['react-refresh', '@typescript-eslint', 'react'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'no-unused-vars': 'off', // Turn off base rule
     '@typescript-eslint/no-unused-vars': 'warn',
     '@typescript-eslint/no-explicit-any': 'warn',
     'no-undef': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
   },
 }
